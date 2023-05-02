272c5f0fb726d7c7f805dccf3e05725ef6ebda7b
valerio <valerio.coltre@gmail.com>
2023-05-02 20:14:46 +0200

    eth funding works

--- src/commands/fund_eth.command.ts
+++ src/commands/fund_eth.command.ts
@@ -1,10 +1,11 @@
 import { env } from '@/config'
 import { type Address } from '@/types'
 import { type CommandInteraction, EmbedBuilder } from 'discord.js'
+import { utils, providers, Wallet } from 'ethers'
 
 import { URL } from 'url'
 
-const userFundCooldown = async (userId: string, wallet: Address): Promise<number> => {
+const userFundCooldown = async (userId: string): Promise<number> => {
   const url = new URL(`/EXPIRETIME/test:fund_eth-${userId}`, env.DB_URL)
   const request = await fetch(url)
   const response = await request.json()
@@ -14,14 +15,28 @@ const userFundCooldown = async (userId: string, wallet: Address): Promise<number
 }
 
 // NOTE TO SELF: redis commands have an "environment-based" prefix, for now it's "test"
-const setUserFunded = async (userId: string, wallet: Address) => {
+const setUserFunded = async (userId: string, txHash: Address) => {
   const secondsInDay = (60 * 60 * 24).toString()
-  const url = new URL(`/SETEX/test:fund_eth-${userId}/${secondsInDay}/${wallet}`, env.DB_URL)
+  const url = new URL(`/SETEX/test:fund_eth-${userId}/${secondsInDay}/${txHash}`, env.DB_URL)
   try {
     await fetch(url)
   } catch (error) {
     console.log({ msg: 'setUserFunded went into error', error })
   }
+}
+
+const fundWallet = async (destination: Address) => {
+  // Define the transaction parameters
+  const provider = new providers.JsonRpcProvider(env.RPC_URL)
+  const wallet = new Wallet(env.PRIVATE_KEY, provider)
+  const valueToSend = utils.parseEther(env.SEND_ETH_AMOUNT)
+
+  const tx = await wallet.sendTransaction({
+    to: destination,
+    value: valueToSend,
+  })
+
+  return tx.hash
 }
 
 export const handleCommand = async (commandInteraction: CommandInteraction) => {
@@ -46,7 +61,7 @@ export const handleCommand = async (commandInteraction: CommandInteraction) => {
   const safeWallet = wallet.value.toLowerCase() as Address
 
   // handles funding 1 time per day
-  const fundCooldown = await userFundCooldown(member!.user.id, safeWallet)
+  const fundCooldown = await userFundCooldown(member!.user.id)
   if (fundCooldown > 0) {
     const now = new Date()
     const expiryDate = new Date(fundCooldown * 1000)
@@ -64,8 +79,17 @@ export const handleCommand = async (commandInteraction: CommandInteraction) => {
   }
 
   const userId = member!.user.id
-  console.log(`Funding wallet ${safeWallet} of member ${userId} with ${env.SEND_ETH_AMOUNT} ETH.`)
 
-  await setUserFunded(userId, safeWallet)
-  await commandInteraction.reply(`Funding wallet ${safeWallet} with ${env.SEND_ETH_AMOUNT} ETH.`)
+  console.log(`Funding wallet ${safeWallet} of member ${userId} with ${env.SEND_ETH_AMOUNT} ETH.`)
+  const txHash = await fundWallet(safeWallet)
+
+  await setUserFunded(userId, txHash as Address)
+
+  const embedReply = new EmbedBuilder()
+    .setColor(0x74eb34)
+    .setTitle('Success')
+    .setDescription(
+      `Funded wallet \`${safeWallet}\` with ${env.SEND_ETH_AMOUNT} ETH \n [View on Etherscan](https://goerli.etherscan.io/tx/${txHash})`,
+    )
+  await commandInteraction.reply({ embeds: [embedReply] })
 }
